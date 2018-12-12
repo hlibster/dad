287215aaa70d0f855fa89fea7833c018461ad245
Hlibster <hlib.bryskin@gmail.com>
2018-12-12 21:34:34 +0100

    Update

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let div = document.getElementById("buttons");
+let correctAnswerAudio = document.getElementById("correct_answer_audio");
+let incorrectAnswerAudio = document.getElementById("incorrect_answer_audio");
 
 
 div.onclick=function test(event) {
@@ -10,16 +12,20 @@ div.onclick=function test(event) {
 		let option = document.getElementById("option1").innerHTML;
 		if (correct_answer == option) {
 			alert("Correct!");
+			correctAnswerAudio.play();
 		} else {
 			alert("Wrong!");
+			incorrectAnswerAudio.play();
 		}
 	} 
 	else if (target.classList[1] == "option2") {
 		let option = document.getElementById("option2").innerHTML;
 		if (correct_answer == option) {
 			alert("Correct!");
+			correctAnswerAudio.play();
 		} else {
 			alert("Wrong!");
+			incorrectAnswerAudio.play();
 		}
 	}
 }
